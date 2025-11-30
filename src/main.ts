5bedc5ef230886f025eefcd32215e75abfbb79fb
faresmohamedrocka <faresmohamed.rockaidev@gmail.com>
2025-11-30 17:04:07 +0200

    Make User To Upload Image For Cloudinary

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,21 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
-
+import { json, urlencoded } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-
-
   app.use(helmet());
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
 
   // ValidationPipe عالمي
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,           
-      forbidNonWhitelisted: true, 
-      transform: true,            
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
